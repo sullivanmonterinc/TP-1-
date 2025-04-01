48d2416ccbcb55331248a960ce514acf832e30c8
Paredez Mateo <paredezmarta46@gmail.com>
2025-04-01 11:11:32 -0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1;
+    int n2;
+    int n3;
+    cout<<"ingrese el primer numero";
+    cin>>n1;
+    cout<<"ingrese el segundo numero";
+    cin>>n2;
+    cout<<"ingrese el tercer numero";
+    cin>>n3;
+    
+    if(n1<10&&n2<10&&n3<10){
+        cout<<"todos los numeros son menores a 10";
+    }
+    else{
+        cout<<"todos los numeros no son menores a 10";
+    }
+return 0;
+
+}
+
+    
